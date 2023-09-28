b97545280b32075c70835760b43880ade5de9f8f
nishikaru <mdsajed001@gmail.com>
2023-09-28 19:12:57 +0600

    favicon getting from google api

--- src/component/FastLink.tsx
+++ src/component/FastLink.tsx
@@ -12,7 +12,9 @@ export const FastLink = ({ item }: fastLinkPropType) => {
     <a href={item?.url}>
       <div className="w-12 h-12 border-2 flex items-center justify-center border-orange-500 rounded-lg relative bg-slate-700">
         <div
-          style={{ backgroundImage: `url(${item.url}/favicon.ico)` }}
+          style={{
+            backgroundImage: `url(https://www.google.com/s2/favicons?domain=${item.url}&sz=64)`,
+          }}
           className="w-6 h-6 rounded-lg bg-cover bg-center z-10"
         ></div>
         <div className="uppercase font-bold text-white flex items-center justify-center w-8 h-8 rounded-md bg-cover bg-center bg-slate-700 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] z-0">
